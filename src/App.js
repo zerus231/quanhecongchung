73c907264b30ccaa7a82f89d79a06f2729fe7c60
LocNT <kusmile15@gmail.com>
2024-11-10 14:13:31 +0700

    commit new

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
-import file from './file.pdf';
-// Text layer for React-PDF.
-import 'react-pdf/dist/Page/TextLayer.css';
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
 
 const App = () => {
-  const [numPages, setNumPages] = useState(null);
-
-  const onDocumentLoadSuccess = ({ numPages }) => {
-    setNumPages(numPages);
-  };
-
   return (
     <div>
-      <Document
-        file={file}
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
-        {Array.from({ length: numPages }, (_, index) => (
-          <Page key={index + "ádasd"} pageNumber={index + 1} />
-        ))}
-      </Document>
+      {
+        Array.from({ length: 24 }, (_, index) => (
+          <p className='wrap-img'><img src={`/pdf/img${index + 1}.jpg`} alt="" /></p>
+        ))
+      }
     </div>
   );
 };
